2649b60e285f9bea79be94cfc0160d2aa0871e9d
JumaRomano <mzitohcreatives@gmail.com>
2024-12-08 20:14:11 +0300

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class  Comment(models.Model):
+    post = models.ForeignKey(Post ,related_name='comments'. on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk':self.post.pk})
